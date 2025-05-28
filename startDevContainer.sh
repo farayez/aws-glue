00fc3ed39c1c9477dbb379165f48345f3be5a308
Araf Farayez <araf.farayez@gmail.com>
2025-05-29 04:10:47 +0600

    fix credential variable name

--- startDevContainer.sh
+++ startDevContainer.sh
@@ -20,11 +20,11 @@ fi
 
 # Extract AWS credentials for the specified profile
 export AWS_ACCESS_KEY_ID=$(awk -F '=' '/^\['$profile'\]/{p=1}p&&/aws_access_key_id/{print $2;exit}' $credentials_file)
-export AWS_SECRET_KEY=$(awk -F '=' '/^\['$profile'\]/{p=1}p&&/aws_secret_access_key/{print $2;exit}' $credentials_file)
+export AWS_SECRET_ACCESS_KEY=$(awk -F '=' '/^\['$profile'\]/{p=1}p&&/aws_secret_access_key/{print $2;exit}' $credentials_file)
 
 
-# echo "$profile $AWS_ACCESS_KEY_ID $AWS_SECRET_KEY"
-if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_KEY" ]; then
+# echo "$profile $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY"
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
     echo "Error: AWS credentials not found for profile '$profile'."
     exit 1
 fi
@@ -36,7 +36,7 @@ docker run -d -it --rm --user root \
     -e AWS_PROFILE=$profile \
     -e AWS_CONFIG_FILE=/home/hadoop/workspace/.aws/config \
     -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-    -e AWS_SECRET_KEY="$AWS_SECRET_KEY" \
+    -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
     --name glue5_pyspark \
     public.ecr.aws/glue/aws-glue-libs:5 \
     pyspark
